397599846e18cd8666d15d78e8f8365493979778
Kalendervogel <reim.julius@gmx.de>
2025-06-26 23:03:32 +0200

    Refactor image loading and enhance Fenster class with countdown timer and dynamic button behavior

--- ArbeitsUmKordienierer.java
+++ ArbeitsUmKordienierer.java
@@ -5,17 +5,16 @@ import java.io.IOException;
 import javax.imageio.*;
 
 public class ArbeitsUmKordienierer {
-    public static BufferedImage loadmage(String Bildaufbewahrer ) {
-    BufferedImage f1= null;
-    String userDierectory = new File("").getAbsolutePath();
+    public static String programmOrdner = new File("").getAbsolutePath(); //Gibt den Pfad zu dem Ordner in dem das Java Program läuft
+
+    public static BufferedImage loadmage(String bildPfad ) { //Pfad von Bild
+        BufferedImage f1= null;
         try{
-            f1=ImageIO.read(new File(userDierectory+Bildaufbewahrer ) );
+            f1=ImageIO.read(new File(programmOrdner+bildPfad));//Ziehtssich das Bild aus dem Pfad
         }
         catch(IOException e){
             e.printStackTrace();
         }
-
         return f1;
-    }
-    
+    } 
 }
--- Fenster.java
+++ Fenster.java
@@ -5,67 +5,48 @@ import java.awt.event.*;
 
 public class Fenster extends JFrame {
    private int pixelDistance =200; //Distanc die die Maus von dem X entfernt ist bevor teleport
-   int zeit = 60;
+   int zeit = 60; //Zeit bis Bluescreen
     
     public Fenster()
     {
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        setAlwaysOnTop(true);
-        setTitle(" ");
-        setUndecorated(true);
-        setSize(480,350);
-        setIconImage(ArbeitsUmKordienierer.loadmage("/rickrollicon.jpg"));
-        String userDirectory = new java.io.File("").getAbsolutePath();
-        ImageIcon gif = new ImageIcon(userDirectory + "/rickroll.gif");
-        JLabel gifContainer= new JLabel(gif); 
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); //Verhindert das schliesen des Fensters
+        setAlwaysOnTop(true);//Sorgt dafür das dass Fenster über allem ist
+        setUndecorated(true);//sorgt dafür das Keine obere Leiste mehr existiert
+        setSize(480,350);//sezt die größe
+        setIconImage(ArbeitsUmKordienierer.loadmage("/rickrollicon.jpg"));//Setz ein anderes Icon
+        ImageIcon gif = new ImageIcon(ArbeitsUmKordienierer.programmOrdner + "/rickroll.gif");
+        JLabel gifContainer= new JLabel(gif);//Macht den Rickrollgif in das Fenster
+        this.getContentPane().add(gifContainer);
         gifContainer.setHorizontalAlignment(JLabel.CENTER);
         gifContainer.setVerticalAlignment(JLabel.BOTTOM);
         gifContainer.setSize(new Dimension(getWidth(), getHeight() - 40));
-        ImageIcon schliessen = new ImageIcon(userDirectory + "/schliessen.png");
-        JButton schliessenKnopf = new JButton();
-        schliessenKnopf.addActionListener(new ActionListener() {  //Wenn man ehinkriegt denn Knopf zu drücken kommt ein weiteres Fenster
+        ImageIcon schliessen = new ImageIcon(ArbeitsUmKordienierer.programmOrdner + "/schliessen.png"); //Macht das schliesbuten Image
+        JButton schliessenKnopf = new JButton(schliessen);
+        this.getContentPane().add(schliessenKnopf);
+        schliessenKnopf.setSize(40, 40);
+        schliessenKnopf.setLocation(getWidth()-45, 5);
+
+
+        schliessenKnopf.addActionListener(new ActionListener() {  //Wenn man es hinkriegt denn Knopf zu drücken kommt ein weiteres Fenster
           @Override
-          public void actionPerformed(ActionEvent e) {
+          public void actionPerformed(ActionEvent e) {//e=sin Informationen über die Action
               new Fenster();
           }  
         });
-        schliessenKnopf.setIcon(schliessen);
-        schliessenKnopf.setSize(40, 40);
-        schliessenKnopf.setLocation(getWidth()-45, 5);
-        JLabel timerDisplay = new JLabel("60");
+       
+        JLabel timerDisplay = new JLabel("60");//Mact den Timer
         timerDisplay.setLocation(10,10);
         timerDisplay.setFont(new Font("Arial", Font.BOLD, 24));
-       
         timerDisplay.setSize(50, 20);
         this.getContentPane().add(timerDisplay);
-        this.getContentPane().add(schliessenKnopf);
-        this.getContentPane().add(gifContainer);
         timerDisplay.setForeground(Color.BLUE);
         
-        
-        addMouseMotionListener(new MouseMotionAdapter() {
-            @Override
-            public void mouseMoved(MouseEvent e){
-                Point mausPosition = e.getLocationOnScreen();
-                Point fensterPosition = getLocationOnScreen();
-                Point schließenKnopf = new Point(fensterPosition.x + getWidth() - 10, fensterPosition.y + 10);
-                double distance = mausPosition.distance(schließenKnopf);
-
-                if(distance < pixelDistance){
-                    Dimension bildschirmGröße = Toolkit.getDefaultToolkit().getScreenSize();
-                    int neueXposition = (int) (Math.random() * (bildschirmGröße.width - getWidth()));
-                    int neueYposition = (int) (Math.random() * (bildschirmGröße.height - getHeight()));
-                    setLocation(neueXposition, neueYposition);                }
-
-            }  
-        });
-
-        Timer timer = new Timer(1000,new ActionListener() {
+        Timer timer = new Timer(1000,new ActionListener() {//Gibt jede Sekunde eine Action
             public void actionPerformed(ActionEvent e){
                 zeit--;
                 
-                timerDisplay.setText(Integer.toString(zeit));
-                if (zeit<=0) {
+                timerDisplay.setText(Integer.toString(zeit));//setz den text vom timer
+                if (zeit<=0) {                               //Führt den Bluescreen aus und Stopt den Timer
                     ((Timer) e.getSource()).stop();
                    
                     Bluescreen.dasEnde();
@@ -76,6 +57,25 @@ public class Fenster extends JFrame {
         
         timer.start();
         setVisible(true);
+
+        addMouseMotionListener(new MouseMotionAdapter() { //sorgt dafür das sich das Fenster weg tpt wenn die Maus zu nah kommt
+            @Override
+            public void mouseMoved(MouseEvent e){
+                Point mausPosition = e.getLocationOnScreen();
+                Point fensterPosition = getLocationOnScreen();
+                Point schließenKnopf = new Point(fensterPosition.x + getWidth() - 10, fensterPosition.y + 10);
+                double distance = mausPosition.distance(schließenKnopf);//Ermittelt die distance zwischen Maus und Schliesknopf
+
+                if(distance < pixelDistance){ //Teleportiert das fenster an einen anderen Punkt
+                    Dimension bildschirmGröße = Toolkit.getDefaultToolkit().getScreenSize();
+                    int neueXposition = (int) (Math.random() * (bildschirmGröße.width - getWidth()));
+                    int neueYposition = (int) (Math.random() * (bildschirmGröße.height - getHeight()));
+                    setLocation(neueXposition, neueYposition);                }
+
+            }  
+        });
+
+
 
     }
 }
